eb786bba73b578efa6e5fb8b068ac963ac9a8d0a
ananjaser1211 <ananjaser@gmail.com>
2018-08-25 03:09:12 +0400

    Update to EliteTuner
    - New icons
    - Package name
    - Updater
    
    and bunch more
    
    Signed-off-by: ananjaser1211 <ananjaser@gmail.com>

--- app/src/androidTest/java/com/moro/mtweaks/ApplicationTest.java
+++ app/src/androidTest/java/com/moro/mtweaks/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.moro.mtweaks;
+package com.elite.elitetuner;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/bvalosek/cpuspy/CpuSpyApp.java
+++ app/src/main/java/com/bvalosek/cpuspy/CpuSpyApp.java
@@ -11,8 +11,8 @@ package com.bvalosek.cpuspy;
 import android.content.Context;
 import android.util.SparseArray;
 
-import com.moro.mtweaks.utils.AppSettings;
-import com.moro.mtweaks.utils.Utils;
+import com.elite.elitetuner.utils.AppSettings;
+import com.elite.elitetuner.utils.Utils;
 
 /**
  * main application class
