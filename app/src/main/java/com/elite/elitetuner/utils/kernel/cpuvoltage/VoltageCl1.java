279e4b174c503924bae18e215a963e9fb095cc8e
ananjaser1211 <ananjaser@gmail.com>
2018-10-02 22:13:25 +0400

    voltage: Adjust for exynos5433

--- app/src/main/java/com/elite/elitetuner/utils/kernel/cpuvoltage/VoltageCl1.java
+++ app/src/main/java/com/elite/elitetuner/utils/kernel/cpuvoltage/VoltageCl1.java
@@ -38,7 +38,7 @@ public class VoltageCl1 {
 
     public static final String BACKUP = "/data/.mtweaks/cpuCl1_stock_voltage";
 
-    public static final String CL1_VOLTAGE = "/sys/devices/system/cpu/cpufreq/mp-cpufreq/cluster1_volt_table";
+    public static final String CL1_VOLTAGE = "/sys/devices/system/cpu/cpufreq/mp-cpufreq/kfc_volt_table";
 
     private static final HashMap<String, Boolean> sVoltages = new HashMap<>();
     private static final HashMap<String, Integer> sOffset = new HashMap<>();
